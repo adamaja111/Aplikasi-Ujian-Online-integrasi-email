e29f91f32632b8dd36ea4bcc07cc4bceac05469c
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2026-05-17 21:57:42 +0800

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import prisma from '@/lib/prisma'
 import { LandingForm } from '@/components/landing-form'
 
+export const revalidate = 0; // Force fresh data
 export const dynamic = 'force-dynamic'
 
 export default async function Home() {
